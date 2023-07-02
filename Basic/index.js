aafedc0dc166cc8a66b90a01270ee9706ff0ee75
Rafsan12 <rafsan.web.2022@gmail.com>
2023-07-02 10:03:28 +0600

    basic-dom

--- Basic/index.js
+++ Basic/index.js
@@ -6,9 +6,17 @@ console.log("Hellow World")
 
 **use getElementsByTagName you got all html tag. 
 
-**if you want any specific element in html then you should use getElementById
+**if you want any specific element in html then you should use getElementById.its given dircet element
 
-**if you need more than one elements in html you should you getElementsByclass
+**if you need more than one elements in html you should you getElementsByclass.its given collection of elements.
+
+**querySelector
+**querySelectorAll
+**classList
+**getAttribute
+**setAttribute
+**innerHtml
+**innerText
 */
 
 const liCollection = document.getElementsByTagName("li");
